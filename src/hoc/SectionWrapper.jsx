258597286e43adf2a8d075dda33692cd1e27e52c
Brian Bissette <bbissette3@yahoo.com>
2023-06-21 11:11:34 -0500

    started experience

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -13,8 +13,8 @@ const SectionWrapper = (Component, idName) =>
         viewport={{ once: true, amount: 0.25 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
-        <span className="has-span" id={idName}>
-          &npsp;
+        <span className="hash-span" id={idName}>
+          &nbsp;
         </span>
         <Component />
       </motion.section>
