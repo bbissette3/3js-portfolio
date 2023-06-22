f1436726fb764f1ae439e96a2a3933c4e7a0c027
Brian Bissette <bbissette3@yahoo.com>
2023-06-22 14:47:02 -0500

    moved stars to the entire page

--- src/App.jsx
+++ src/App.jsx
@@ -18,19 +18,19 @@ const App = () => {
   return (
     <BrowserRouter>
       <ToastContainer theme="dark" />
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
-        </div>
+      <div className="relative z-10 bg-primary">
+        <StarsCanvas />
+        {/* <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"> */}
+        <Navbar />
+        <Hero />
+        {/* </div> */}
         <About />
         {/* <Experience /> */}
         <Tech />
         <Works />
         {/* <Feedbacks /> */}
-        <div className="relative z-0">
+        <div className="relative">
           <Contact />
-          <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -6,11 +6,11 @@ import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
-  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+  const sphere = random.inSphere(new Float32Array(8000), { radius: 2 });
 
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
+    ref.current.rotation.x -= delta / 40;
+    ref.current.rotation.y -= delta / 50;
   });
 
   return (
@@ -19,7 +19,7 @@ const Stars = (props) => {
         <PointMaterial
           transparent
           color="#f272c8"
-          size={0.002}
+          size={0.001}
           sizeAttenuation={true}
           depthWrite={false}
         />
@@ -30,8 +30,8 @@ const Stars = (props) => {
 
 const StarsCanvas = () => {
   return (
-    <div className="w-full h-auto absolute inset-0 z-[-1]">
-      <Canvas camera={{ position: [0, 0, 1] }}>
+    <div className="w-full h-full absolute inset-0 z-[-10]">
+      <Canvas camera={{ position: [0, 0, 2] }}>
         <Suspense fallback={null}>
           <Stars />
         </Suspense>
